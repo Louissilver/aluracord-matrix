191754115334a792e8b7588da8ad7dec05614763
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2022-01-25 13:28:21 -0300

    Aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,10 @@
 import { } from '@skynexui/components';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useState } from 'react';
 import appConfig from '../config.json';
+import { useRouter } from 'next/router';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -48,11 +23,11 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'louissilver';
+  const [username, setUsername] = useState('louissilver');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -79,6 +54,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (event) {
+              event.preventDefault();
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -91,6 +70,11 @@ export default function PaginaInicial() {
 
             <TextField
               fullWidth
+
+              value={username}
+              onChange={function (event) {
+                setUsername(event.target.value);
+              }}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -103,6 +87,7 @@ export default function PaginaInicial() {
             <Button
               type='submit'
               label='Entrar'
+
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
@@ -131,13 +116,13 @@ export default function PaginaInicial() {
               minHeight: '240px',
             }}
           >
-            <Image
+            {username.length <= 2 || <Image
               styleSheet={{
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
               src={`https://github.com/${username}.png`}
-            />
+            />}
             <Text
               variant="body4"
               styleSheet={{
